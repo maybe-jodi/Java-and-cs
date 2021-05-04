6fd1869f0ca5d454a55797d54abc506985c09a11
NTBeta <78397696+NTBeta@users.noreply.github.com>
2021-05-03 20:52:54 -0500

    Added something

---
+++ NTPCStuff.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Baseline Bench: Game 34%, Desk 71%, Work 28%
+CPU: AMD Athlon 3000G $49
+GPU: AMD RX 570 $150
+SSD: Kingston A400 240GB $30
+HDD: WD Blue 1TB (2012) $35
+RAM: Corsair Vengeance LPX DDR4 3200 C16 2x8GB $79
+MBD: Asus PRIME A320M-K $65
+Total: $408
+
+Alternative Bench: Game 19%, Desk 74%, Work 16%
+CPU: AMD Ryzen 3 3200G $159
+GPU: Nvidia GT 1030
+SSD: Kingston A400 240GB $30
+HDD: WD Blue 1TB (2012) $35
+RAM: Corsair Vengeance LPX DDR4 3200 C16 2x8GB $79
+MBD: Asus PRIME A320M-K $65
+Total: $368
+");
+  }
+}
